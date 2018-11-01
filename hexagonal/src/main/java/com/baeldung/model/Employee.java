6cd8806b5ffc9c3277b64897aeda8c8ca3bd7bc2
codevaibhav <vaibhav.bansal27@gmail.com>
2018-11-01 20:12:06 +0530

    db settings, logging, file save

--- hexagonal/src/main/java/com/baeldung/model/Employee.java
+++ hexagonal/src/main/java/com/baeldung/model/Employee.java
@@ -2,11 +2,22 @@ package com.baeldung.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Employee")
 public class Employee implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -6365451485741046756L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name; 
 	private String address;
---
+++ hexagonal/src/main/java/com/baeldung/repo/DatabaseEmployeeRepository.java
@@ -0,0 +1,40 @@
+package com.baeldung.repo;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.baeldung.domain.EmployeeRepository;
+import com.baeldung.model.Employee;
+
+public class DatabaseEmployeeRepository implements EmployeeRepository{
+	
+	 private static final String PERSISTENCE_UNIT_NAME = "Employee";
+     private static EntityManagerFactory factory;
+
+	
+    @Override
+    public Employee saveEmployee(Employee emp) {
+    	EntityManager em = factory.createEntityManager();
+    	em.getTransaction().begin();
+        
+        em.persist(emp);
+        em.getTransaction().commit();
+
+        em.close();
+        return emp;
+    }
+    
+    private DatabaseEmployeeRepository(){
+    	factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+    }
+    
+    public static DatabaseEmployeeRepository getInstance(){
+		return DatabaseEmployeeRepositorySubClass.INSTANCE;
+	}
+	
+	private static class DatabaseEmployeeRepositorySubClass{
+		
+		private static DatabaseEmployeeRepository INSTANCE = new DatabaseEmployeeRepository();
+	}
+}
